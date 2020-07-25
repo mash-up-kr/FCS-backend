1c84638217c40cbeabff888994ca9679f086aac5
sdh2398 <ehdgus2398@gmail.com>
2020-07-25 18:00:49 +0900

    Style Entity 생성, Json 설정 추가

---
+++ src/main/java/com/mashup/ootd/domain/style/domain/Style.java
@@ -0,0 +1,40 @@
+package com.mashup.ootd.domain.style.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode(of = "id")
+@Getter
+@Entity
+public class Style {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	private void onInit() {
+		LocalDateTime now = LocalDateTime.now();
+		this.createdAt = now;
+		this.updatedAt = now;
+	}
+
+	@PreUpdate
+	private void preUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+}
---
+++ src/main/java/com/mashup/ootd/domain/style/domain/UserStyle.java
@@ -0,0 +1,50 @@
+package com.mashup.ootd.domain.style.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import com.mashup.ootd.domain.user.entity.User;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode(of = "id")
+@Getter
+@Entity
+public class UserStyle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "style_id")
+	private Style style;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	private void onInit() {
+		LocalDateTime now = LocalDateTime.now();
+		this.createdAt = now;
+		this.updatedAt = now;
+	}
+
+	@PreUpdate
+	private void preUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+}
