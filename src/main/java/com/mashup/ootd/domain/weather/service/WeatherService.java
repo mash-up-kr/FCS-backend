8fc94dc3303c21069a346dcdd9594332a8e73b28
secureWKkim <1rladnjsrud1@gmail.com>
2020-09-08 22:43:22 +0900

    Reverse Geo Location API 호출 및 파싱 완료

--- src/main/java/com/mashup/ootd/domain/weather/service/WeatherService.java
+++ src/main/java/com/mashup/ootd/domain/weather/service/WeatherService.java
@@ -1,11 +1,8 @@
 package com.mashup.ootd.domain.weather.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mashup.ootd.domain.location.entity.Location;
 import com.mashup.ootd.domain.weather.domain.PostWeather;
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -25,6 +22,7 @@ import java.util.*;
 public class WeatherService {
     // 사실상 current weather 만 불러옴
     PostWeather postWeather = new PostWeather();
+    StringBuilder location = new StringBuilder();
 
     public PostWeather getPostWeatherInfo(String lat, String lon) {
         try {
@@ -36,14 +34,14 @@ public class WeatherService {
             HttpHeaders header = new HttpHeaders(); //header? body?
             HttpEntity<?> entity = new HttpEntity<>(header);
             //원랜 dt= 를 호출해서 historical date 를 넣어줘야함. 지금 포스트는 무저건 현재 시각으로만 생성 가능하게 했으므로 후에 리팩토링
-            String apiURL = new StringBuilder().append("https://api.openweathermap.org/data/2.5/onecall?").append("lat=").append(lat).append("&lon=").append(lon).append("&exclude=minutely,hourly,daily").append("&appid=b692c24a9d64e29901f2fa8c899d1b9f").toString();
+            String apiURL = new StringBuilder().append("https://api.openweathermap.org/data/2.5/onecall?").append("lat=").append(lat).append("&lon=").append(lon).append("&exclude=minutely,hourly,daily").append("&appid=YourApiKey").toString();
             UriComponents uri = UriComponentsBuilder.fromHttpUrl(apiURL).build();
 
             ResponseEntity<Map> postWeatherResponseEntity = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
 
             JSONObject bigObj = new JSONObject(Objects.requireNonNull(postWeatherResponseEntity.getBody()));
             LinkedHashMap smallObj = (LinkedHashMap) bigObj.get("current");
-            //temp는 원래 double 타입으로 전달
+            //temp 는 원래 double 타입으로 전달
             double element = (double) smallObj.get("temp");
             String convertedElement = Double.toString(element);
             postWeather.setApparentTemp(convertedElement);
@@ -75,31 +73,32 @@ public class WeatherService {
             RestTemplate restTemplate = new RestTemplate(factory);
 
             HttpHeaders header = new HttpHeaders(); //header? body?
+            header.set("X-NCP-APIGW-API-KEY-ID", "YourClientID");
+            header.set("X-NCP-APIGW-API-KEY","YourAPIKey");
             HttpEntity<?> entity = new HttpEntity<>(header);
-            //원랜 dt= 를 호출해서 historical date 를 넣어줘야함. 지금 포스트는 무저건 현재 시각으로만 생성 가능하게 했으므로 후에 리팩토링
-            String apiURL = new StringBuilder().append("https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc").append("request=coordsToaddr&coords=").append(lat).append("&orders=legalcode&output=json").toString();
+            //여기에 몬가 setRequestProperty 를 통해 토큰을 헤더에 조작해준 뒤 들어가는게 필요한데...
+            String apiURL = new StringBuilder().append("https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?").append("request=coordsToaddr&coords=").append(lat).append(",").append(lon).append("&orders=legalcode&output=json").toString();
             UriComponents uri = UriComponentsBuilder.fromHttpUrl(apiURL).build();
 
-            ResponseEntity<Map> postWeatherResponseEntity = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            ResponseEntity<Map> geoInfoResponseEntity = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
 
-            JSONObject bigObj = new JSONObject(Objects.requireNonNull(postWeatherResponseEntity.getBody()));
-            LinkedHashMap smallObj = (LinkedHashMap) bigObj.get("current");
-            //temp는 원래 double 타입으로 전달
-            double element = (double) smallObj.get("temp");
-            String convertedElement = Double.toString(element);
-            postWeather.setApparentTemp(convertedElement);
+            JSONObject bigObj = new JSONObject(Objects.requireNonNull(geoInfoResponseEntity.getBody()));
+            ArrayList geoObjList = (ArrayList) bigObj.get("results");
+            LinkedHashMap smallObj = (LinkedHashMap) geoObjList.get(0);
+            LinkedHashMap areaList = (LinkedHashMap) smallObj.get("region");
+            smallObj = (LinkedHashMap) areaList.get("area1");
+            location.append(smallObj.get("name")).append(" "); //"경기도"
 
-            element = (double) smallObj.get("feels_like");
-            convertedElement = Double.toString(element);
-            postWeather.setWindChillTemp(convertedElement);
+            smallObj = (LinkedHashMap) areaList.get("area2");
+            location.append(smallObj.get("name")).append(" "); //"성남시 분당구"
 
-            //weather 안엔 제이슨 배열이 오므로(제이슨 여러개) 이거 처리법 연구해보삼
-            ArrayList weatherObjList = (ArrayList) smallObj.get("weather");
-            smallObj = (LinkedHashMap) weatherObjList.get(0);
-            convertedElement = (String) smallObj.get("main");
-            postWeather.setDescription(convertedElement);
+            smallObj = (LinkedHashMap) areaList.get("area3");
+            location.append(smallObj.get("name")).append(" "); //"정자동"
 
-            return postWeather;
+            smallObj = (LinkedHashMap) areaList.get("area4");
+            location.append(smallObj.get("name"));
+
+            return location.toString();
         } catch (Exception e) {
             System.out.println(e.toString());
         }
--- src/test/java/com/mashup/ootd/domain/WeatherServiceTest.java
+++ src/test/java/com/mashup/ootd/domain/WeatherServiceTest.java
@@ -14,10 +14,19 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @RunWith(SpringRunner.class)//이것도 종류 검색좀...
 @RestClientTest(WeatherService.class)
 public class WeatherServiceTest {
+    /*
     @Test
     public void getPostWeatherInfoTest() {
         WeatherService weatherService = new WeatherService();
         PostWeather result = weatherService.getPostWeatherInfo("40.12", "-96.66");
         System.out.println(result);
+    }
+     */
+
+    @Test
+    public void getGeoInfoTest() {
+        WeatherService weatherService = new WeatherService();
+        String location = weatherService.getLocationInfo("127.1054065","37.3595669");
+        System.out.println(location);
     }
 }
