b77239b56defc636cf75a0c42b0b767cae2f5038
sdh2398 <ehdgus2398@gmail.com>
2020-09-10 00:49:28 +0900

    CORS 설정 추가

--- src/main/java/com/mashup/ootd/config/WebConfig.java
+++ src/main/java/com/mashup/ootd/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,17 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.mashup.ootd.web.interceptor.JwtInterceptor;
 import com.mashup.ootd.web.resolver.UserHandlerMethodArgumentResolver;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Autowired
 	private JwtInterceptor jwtInterceptor;
-	
+
 	@Autowired
 	private UserHandlerMethodArgumentResolver userHandlerMethodArgumentResolver;
 
-	private static final String[] EXCLUDE_PATHS = { "/api/ping", "/api/users/sign-up", "/api/users/sign-in", "/api/styles" };
+	private static final String[] EXCLUDE_PATHS = { "/docs/**", "/api/ping", "/api/users/sign-up", "/api/users/sign-in",
+			"/api/styles" };
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -35,4 +36,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(userHandlerMethodArgumentResolver);
 	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+	
 }
