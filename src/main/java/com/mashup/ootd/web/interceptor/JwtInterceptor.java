cbd09ca9815ea1f85b2412762f0848cf6b1edb1d
sdh2398 <ehdgus2398@gmail.com>
2020-09-11 02:23:07 +0900

    JwtInterceptor에 HTTP OPTION 메소드는 JWT 검증로직 안타도록 수정
    - CORS 문제로 인해 추가

--- src/main/java/com/mashup/ootd/web/interceptor/JwtInterceptor.java
+++ src/main/java/com/mashup/ootd/web/interceptor/JwtInterceptor.java
@@ -10,7 +10,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.mashup.ootd.domain.jwt.service.JwtService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
@@ -20,6 +22,12 @@ public class JwtInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+
+		if (request.getMethod().equals("OPTIONS" )) {
+			log.debug("if request options method is options, return true");
+
+			return true;
+		}
 
 		String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
 
